76dd1c2a77739ca2c5281d6e881601f075e18fb6
Msieur Gab <73283+msieur-gab@users.noreply.github.com>
2024-07-17 09:32:27 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,12 @@
 export const CONFIG = {
     DEBUG: true,
-    DETECTION_DURATION: 15000, // 20 seconds
+    DETECTION_DURATION: 20000, // 20 seconds
     SOUNDS_PATH: './sounds/',
     FPS: 30,
-    USE_FLASH: true
+    USE_FLASH: true,
+    BPM_ANALYZER: {
+        MIN_VARIATION_THRESHOLD: 2,
+        PEAK_THRESHOLD: 0.5,
+        LOW_PASS_ALPHA: 0.1
+    }
 };
